47f42dcbcbc21ff19bc6edd6a933ebc19928ceb4
jlchiam <joelne_chiam@rp.edu.sg>
2022-03-08 22:09:47 +0800

    backend commit

--- backend/routers.js
+++ backend/routers.js
@@ -5,7 +5,8 @@ let router = express.Router();
 require('dotenv').config();
 
 const { auth, requiresAuth } = require('express-openid-connect');
-const Connection = require("mysql/lib/Connection");
+const { connection } = require("./database");
+
 router.use(
   auth({
     //auth0_domain: 'dev-8xccpmo6.us.auth0.com',
@@ -19,10 +20,12 @@ router.use(
   })
 );
 
+/*
 router.get("/home", (request, response) => {
     response.send("Welcome to Dev Toolkit 2!");
 })
-
+*/
+/*
 router.get('/', (request, response) => {
     response.send(request.oidc.isAuthenticated() ? 'Logged in' : 'Logged out')
   });
@@ -30,11 +33,99 @@ router.get('/', (request, response) => {
   router.get('/profile', requiresAuth(), (request, response) => {
       response.send(JSON.stringify(request.oidc.user));
   });
-  
-router.get('/user/by-uid', requiresAuth(), (request, response) => {
-      let user = data.get_user_by_user_id(request.query.user_id);
-      response.status(200).send(user);
+*/
+
+/*
+router.get("/users/all", (request, response) => {
+  // let users = database.get_all_users(); //users variable is only defined in this area, for the other router.get with let users, it is considered another area
+  // response.send(users);
+  connection.query("select * from user", (errors, results) => {
+      if (errors) {
+          console.log(errors);
+          response.status(500).send("Something went wrong...");
+      } else {
+          response.status(200).send(results);
+      }
+  });
+});
+*/
+
+router.post("/user/add", (request, response) => {
+  connection.query(
+      `INSERT INTO user (first_name, last_name, email) 
+      VALUES ("${request.body.fname}", 
+      "${request.body.lname}", 
+      "${request.body.email}")`, 
+      (errors,results)=>{
+        if (errors) {
+          console.log(errors);
+          response.status(500).send("Something went wrong...");
+        } else {
+          response.status(200).send("User added to the database!");
+        }
+      }
+    );
+
+});
+
+
+  router.get("/Workprofile/all", (request, response) => {
+    connection.query("SELECT * FROM Workprofile", (errors, results) => {
+        if (errors) {
+            console.log(errors);
+            response.status(500).send("Something went wrong...");
+        } else {
+            response.status(200).send(results);
+        }
     });
+});
+
+router.get("/Userprofile/all", (request, response) => {
+  connection.query("SELECT * FROM Userprofile", (errors, results) => {
+      if (errors) {
+          console.log(errors);
+          response.status(500).send("Something went wrong...");
+      } else {
+          response.status(200).send(results);
+      }
+  });
+});
+
+
+router.post("/Userprofile/add", (request, response) => {
+   connection.query(
+      `INSERT INTO Userprofile (first_name, last_name,
+        email_address, work_profile_id, monthly_income, 
+        risk_appetite, retire_by,
+        answer1, answer2, answer3, answer4,
+        savings, spendings, investments) 
+      values ("${request.body.first_name}", 
+      "${request.body.last_name}", 
+      "${request.body.email_address}",
+      "${request.body.work_profile_id}",
+      "${request.body.monthly_income}",
+      "${request.body.risk_appetite}",
+      "${request.body.retire_by}",
+      "${request.body.answer1}",
+      "${request.body.answer2}",
+      "${request.body.answer3}",
+      "${request.body.answer4}",
+      "${request.body.savings}",
+      "${request.body.spendings}",
+      "${request.body.investments}",
+      )`,
+      (errors,results)=>{
+        if (errors) {
+          console.log(errors);
+          response.status(500).send("Something went wrong...");
+        } else {
+          response.status(200).send("User added to the database!");
+        }
+      }
+    );
+
+});
+
 
 module.exports = { router };
   
--- frontend/getdata.js
+++ frontend/getdata.js
@@ -7,7 +7,32 @@ function getFromServer() {
         .catch((error) => console.log(error));
 }
 
-/*
+
+const b1 = document.getElementById("b1");
+b1.addEventListener("click", () => {
+  let user_id = document.getElementById("inputbox").value; //have an inputbox on the frontend index.html file
+  //$.getJSON(
+    // "http://nusbackendstub.herokuapp.com/user/all",
+    //`http://nusbackendstub.herokuapp.com/user/by-uid?user_id=${user_id}`,
+    $.getJSON(`http://localhost:3000/users/by-uid?uid=${user_id}`, (data) => { //data was sent as list - mock api was sending single object
+    data = data[0];  //mysql is a list of object response so what he did is made data = 0 as the 1st element to be taken from that list
+    console.log(data)
+      let code = `First Name: ${data.first_name} <br>
+                Last Name:  ${data.last_name} <br>
+                Email:      ${data.email}`;
+    $(".mypanel").html(code);
+      // let code = "<ul>";
+      // data.forEach((datapoint) => {
+      //   code += `<li> First Name: ${datapoint.first_name}
+      //               Last Name: ${datapoint.last_name}
+      //               Email: ${datapoint.email} </li>`;
+      // });
+      // code += "</ul>";
+      // $(".mypanel").html(code);
+    }
+  );
+});
+
 const b2 = document.getElementById("b2");
 b2.addEventListener("click", () => {
   $.getJSON(`http://localhost:3000/users/all`, (data) => {
@@ -22,4 +47,3 @@ b2.addEventListener("click", () => {
   });
 });
 
-*/
